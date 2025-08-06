c2909887550124abbc6e4e9cf2f80162cea04cc3
GeryTrstno <gerytauristino8@gmail.com>
2025-08-06 08:19:33 +0700

    update

--- LearningApplicantWeb/Models/ApplicantVM.cs
+++ LearningApplicantWeb/Models/ApplicantVM.cs
@@ -1,6 +1,13 @@
-﻿namespace LearningApplicantWeb.Models
+﻿using LearningApplicantWeb.Models.EF;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningApplicantWeb.Models
 {
     public class ApplicantVM
     {
+        public class Index
+        {
+            public List<Applicant> Applicants { get; set; } = new List<Applicant>();
+        }
     }
 }
